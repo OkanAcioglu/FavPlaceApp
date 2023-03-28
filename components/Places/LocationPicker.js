93163f91fbff9ef252c381df662136e66c097ac0
Okan <okanacioglu@gmail.com>
2023-03-29 00:08:29 +0300

    adding-location-preview-map-adding-interactive-map

--- components/Places/LocationPicker.js
+++ components/Places/LocationPicker.js
@@ -1,13 +1,20 @@
-import { View, StyleSheet, Alert } from 'react-native'
+import { View, StyleSheet, Alert, Text, Image } from 'react-native'
 import OutlinedButton from '../UI/OutlinedButton'
+import { useNavigation } from '@react-navigation/native'
 import { Colors } from '../../constants/colors'
 import {
   getCurrentPositionAsync,
   useForegroundPermissions,
   PermissionStatus,
 } from 'expo-location'
+import { useState } from 'react'
+import { getMapPreview } from '../../util/location'
 
 function LocationPicker() {
+  const [pickedLocation, setPickedLocation] = useState()
+
+  const navigation = useNavigation()
+
   const [locationPermissionInformation, requestPermission] =
     useForegroundPermissions()
 
@@ -33,14 +40,31 @@ function LocationPicker() {
       return
     }
     const location = await getCurrentPositionAsync()
-    console.log(location)
+    setPickedLocation({
+      lat: location.coords.latitude,
+      lng: location.coords.longitude,
+    })
   }
 
-  function pickOnMapHandler() {}
+  function pickOnMapHandler() {
+    navigation.navigate('Map')
+  }
 
+  let locationPreview = <Text>No location picked yet!!!</Text>
+
+  if (pickedLocation) {
+    locationPreview = (
+      <Image
+        style={styles.image}
+        source={{
+          uri: getMapPreview(pickedLocation.lat, pickedLocation.lng),
+        }}
+      />
+    )
+  }
   return (
     <View>
-      <View style={styles.mapPreview}></View>
+      <View style={styles.mapPreview}>{locationPreview}</View>
       <View style={styles.actions}>
         <OutlinedButton icon='location' onPress={getLocationHandler}>
           Locate User
@@ -68,5 +92,10 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-around',
     alignItems: 'center',
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+    borderRadius: 4,
   },
 })
